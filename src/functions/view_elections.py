df1667682339212639bc08a9d683ee59c53720a2
KodyRogers <kjr1303@rit.edu>
2025-07-01 23:43:10 -0400

    Update view_elections.py

--- src/functions/view_elections.py
+++ src/functions/view_elections.py
@@ -10,17 +10,19 @@ from psycopg2.extras import RealDictCursor
 def action(curs: RealDictCursor, state: dict):
   if state.get('user') == None:
     print('Not currently logged in')
-    return
+    return False
+  
   curs.execute('''
-      SELECT * FROM postgres.csci455.elections
+      SELECT * FROM postgres.public.elections
   ''')
+
   elections = curs.fetchall()
-  #print(curs.fetchall())
+  print(curs.fetchall())
   for row in elections:
     election_id = row[0]
     print(f"{row[1]} on {row[2]} [{row[3]}]")
     curs.execute(f'''
-        SELECT name, party, position FROM postgres.csci455.candidates WHERE election_id = {election_id}
+        SELECT name, party, position FROM postgres.public.candidates WHERE election_id = {election_id}
     ''')
     candidates = curs.fetchall()
     for candidate in candidate:
